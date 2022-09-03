9fba5c4650d16734b85942ccaa15226825e00ae2
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-03 19:02:50 +0700

    test 6pm

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,20 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  let str = [startWord,word]
+  word = str[1] ?? undefined
+    
+  if (word === null || word === undefined){
+    return undefined
+  }
+  if (word.match(str[0])){
+    return str[1]
+  
+  }else {
+  return str[0]+str[1]
+  }
 }
+console.log(fillStartWord('first','firststep'))
+
+
 module.exports = fillStartWord
---
+++ main.js
@@ -0,0 +1,2 @@
+let str = 'helppppp'
+console.log(str.)
